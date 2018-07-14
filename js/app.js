3bc17cdbf77ae71f2cde64ae8666b8d5589a085a
lucrampro <lucienramassamy@gmail.com>
2018-07-14 18:12:37 +0200

    finalisation de la age MAIS l'animation a bug le mode mobile

---
+++ js/app.js
@@ -0,0 +1,29 @@
+//LES PETIT TRAIS DESSENDE
+TweenMax.to(".trais",0.3,{top:"20%", height:"0"});
+TweenMax.to(".trais2",0.3,{top:"40%", height:"0"});
+TweenMax.to(".trais3",0.3,{top:"60%", height:"0"});
+TweenMax.to(".trais4",0.3,{top:"80%", height:"0"});
+TweenMax.to(".trais5",0.3,{top:"100%", height:"0", onComplete:function(){
+//    LES VOLET S'OUVRE
+    TweenMax.to(".volet",0.5,{width:"0"});
+    TweenMax.to(".volet2",0.5,{width:"0"});
+    TweenMax.to(".volet3",0.5,{width:"0"});
+    TweenMax.to(".volet4",0.5,{width:"0"});
+    TweenMax.to(".volet5",0.5,{width:"0", onComplete: function(){
+        
+        TweenMax.to(".img",0.5,{width:"50%"});                
+        TweenMax.to(".cacheTexte1",0.5,{width:"400px", onComplete(){
+        TweenMax.to(".colorimg",0.5,{width:"0"});
+        TweenMax.to(".colortxt1",0.5,{width:"0"});
+        TweenMax.to("h1",0.5,{opacity:"1"});
+        TweenMax.to(".js",0.5,{opacity:"1"});
+      
+            
+        }});         
+         
+        
+        
+        
+    }});
+    
+}});
